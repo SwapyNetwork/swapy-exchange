b143a05d4c34365fe474473065961a0a5f59946f
Túlio Braga <tuliobragam@gmail.com>
2017-07-03 21:10:19 -0300

    starting angular cli project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
