89c904bf671021360342781ae5467e063fa87944
adrielgs <adrielgusa@gmail.com>
2017-09-11 18:38:42 -0300

    Adding comments from the investment return when one or more assets were not available

--- src/app/investor/invest/invest.component.ts
+++ src/app/investor/invest/invest.component.ts
@@ -24,9 +24,15 @@ export class InvestComponent implements OnInit {
   confirmInvestment(){
   	this.investService.addInvest(this.investment).then(data => {
   		this.investService.cacheInvestment(data.investment);
+      console.log(data);
+      // @todo Show message in case not all of the assets were available.
+      // resolve({ code: "INVADD-E07", message: availableAssetsLength + " out of " + selectedAssetsLength + " assets was available for investment.", investment: params}); -- from User.addInvestment() in backend
 
   		this.router.navigate(["investor/invest/success"]);
   	}, error => {
+      // @todo Implement error page when asset is not available anymore.
+      // reject({ code: "INVADD-E06", message: "Asset not available for investment", investment: params}); -- from User.addInvestment() in backend
+
   		console.log(error);
   	})
   	
