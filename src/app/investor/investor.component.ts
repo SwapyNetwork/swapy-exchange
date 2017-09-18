cdbb242751184419150f30939db2b84ac3949b87
adrielgs <adrielgusa@gmail.com>
2017-09-18 19:06:10 -0300

    Status bar data from investments almost done. Average date payment left

--- src/app/investor/investor.component.ts
+++ src/app/investor/investor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {ViewEncapsulation} from '@angular/core';  
+import { ViewEncapsulation } from '@angular/core';
+import { InvestorService } from './investor.service';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -9,4 +10,24 @@ import {ViewEncapsulation} from '@angular/core';
 })
 export class InvestorComponent {
   title = 'investor';
+
+  public assetsLength;
+  public investedValue;
+  public averageReturn;
+  public returnValue;
+
+  constructor(private investorService: InvestorService) {};
+
+  ngOnInit() {
+  	this.refreshStatusBar();
+  };
+
+  refreshStatusBar(){
+    this.investorService.getMyInvestmnetsInfo().then((data: any) => {
+      this.assetsLength = data.assetsLength;
+      this.investedValue = data.investedValue;
+      this.averageReturn = data.averageReturn;
+      this.returnValue = data.returnValue;
+  	});
+  };
 }
---
+++ src/app/investor/investor.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../common/services/http.service';
+
+@Injectable()
+export class InvestorService {
+
+  constructor(public httpService: HttpService) { }
+
+  getMyInvestmnetsInfo(): Promise<any> {
+  	return this.httpService.get('investment/mine/generalInfo');
+  }
+
+}
