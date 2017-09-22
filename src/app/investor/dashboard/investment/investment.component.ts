a1983335998cd7e1f065f51afa08b389febad22f
Adriel <adrielgusa@gmail.com>
2017-09-22 11:58:11 -0300

    Manage page showing investment summaries

--- src/app/investor/dashboard/investment/investment.component.ts
+++ src/app/investor/dashboard/investment/investment.component.ts
@@ -15,4 +15,18 @@ export class InvestmentComponent implements OnInit {
   ngOnInit() {
   }
 
+  public toggleCollapse() {
+    this.collapsed = !this.collapsed;
+  }
+
+  public calculateReturnAmount(){
+    return this.investment.totalAmount * (1 + this.investment.roi);
+  }
+
+  public calculatePaybackDate() {
+    const paybackDate = new Date(this.investment.investedIn);
+    paybackDate.setMonth(paybackDate.getMonth() + this.investment.paybackMonths);
+    return paybackDate;
+  }
+
 }
