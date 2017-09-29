1b7390ec7ffbbf3099f6fefd0a165ed583d02f85
Adriel <adrielgusa@gmail.com>
2017-09-29 15:36:59 -0300

    Fixing loading screen error at 2FA

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'swapy-web/dist/index.html'),
+    pathname: path.join(__dirname, 'dist/index.html'),
     protocol: 'file:',
     slashes: true
   }));
