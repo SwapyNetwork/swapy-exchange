0af261df6be219cb7084b81889f60d2229ef232d
Adriel <adrielgusa@gmail.com>
2017-10-11 17:35:58 -0300

    HTML changes.Added buttons to transfer funds or decline it

--- src/app/common/interfaces/offerAssetStatus.interface.ts
+++ src/app/common/interfaces/offerAssetStatus.interface.ts
@@ -1,3 +1,11 @@
 export const OPEN = 1;
 export const SOLD = 2;
 export const PENDING = 3;
+//
+export const TX_CREATION_PENDING = 0;
+export const TX_CREATED = 1;
+export const LOCKED = 2 ;
+export const TX_AGREEMENT_PENDING = 3;
+export const TX_AGREED = 4;
+export const TX_INVEST_PENDING = 5;
+export const TX_INVESTED = 6;
