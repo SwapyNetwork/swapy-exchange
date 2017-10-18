7cea2b4b4d3711ab3d376d1a5e6e12ce2f578131
Túlio Braga <tuliobragam@gmail.com>
2017-10-18 02:40:58 -0200

    asar + linux icon

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, icon: 'assets/swapy3.png' });
+  win = new BrowserWindow({ width: 1200, height: 800, icon: 'assets/swapy3.png' });
   const fs = require('fs');
 
   // and load the index.html of the app.
