0c3b215b00680c57597bb6c35c7d61824035a5f8
Adriel <adrielgusa@gmail.com>
2017-10-24 15:37:40 -0200

    Added return buttons on sign-up, terms of service and privacy policy

--- src/app/common/nav/terms-of-service/terms-of-service.component.ts
+++ src/app/common/nav/terms-of-service/terms-of-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'terms-of-service',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsOfServiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
