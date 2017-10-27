0604107d5f718bf38941f74c231334257cb38755
Túlio Braga <tuliobragam@gmail.com>
2017-10-27 17:14:13 -0300

    ignoring env.json when bundling app + minorfix pending-offer

--- src/app/credit-company/pending-offer/pending-offer.component.ts
+++ src/app/credit-company/pending-offer/pending-offer.component.ts
@@ -14,7 +14,7 @@ export class PendingOfferComponent implements OnInit {
   public message: string;
 
   constructor(private walletService: WalletService, private router: Router,
-    private pendingOfferService: PendingOfferService) { }
+    public pendingOfferService: PendingOfferService) { }
 
   ngOnInit() {
     this.pendingOfferService.setMessage(null);
