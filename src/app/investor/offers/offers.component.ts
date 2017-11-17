fee34ccb0b739b3bfbe2a062cf538aa95ba00c55
Adriel <adrielgusa@gmail.com>
2017-11-17 15:27:53 -0200

    Testing new contract import

--- src/app/investor/offers/offers.component.ts
+++ src/app/investor/offers/offers.component.ts
@@ -3,9 +3,8 @@ import { Http } from '@angular/http';
 import { Offer } from '../../common/interfaces/offer.interface';
 import { OfferService } from './offer.service';
 import { Web3Service } from '../../common/services/web3.service';
-import { SwapyExchangeInterface as SwapyExchange } from '../../../../contracts/SwapyExchange';
-import { InvestmentAssetInterface as InvestmentAsset } from '../../../../contracts/InvestmentAsset';
-import { addresses } from '../../../../contracts/address';
+import * as InvestmentAsset from '../../../contracts/InvestmentAsset.json';
+import * as SwapyExchange from '../../../contracts/SwapyExchange.json';
 
 @Component({
   selector: 'app-offers',
@@ -52,62 +51,63 @@ export class OffersComponent implements OnInit {
   }
 
   getOffersFromBlockchain() {
-    const contract = new this.web3.eth.Contract(SwapyExchange.abi, addresses.swapyExchange);
-
-    contract.getPastEvents('Offers', {
-      fromBlock: 0,
-      toBlock: 'latest'
-    }, (error, offersEvents) => {
-      for (const offersEvent of offersEvents) {
-        const contractVariables = offersEvent.returnValues;
-        // console.log(offersEvents);
-        const promises = [];
-        for (const assetAddress of contractVariables._assets) {
-          const contractAsset = new this.web3.eth.Contract(InvestmentAsset.abi, assetAddress);
-          promises.push(new Promise((resolve) => {
-            return contractAsset.methods.value().call().then(value => {
-              return contractAsset.methods.status().call().then(status => {
-                value = this.web3.utils.fromWei(value) * 340; // Temporary fixed value in Alpha
-                const offerAssets = {
-                    value,
-                    status
-                };
-                resolve(offerAssets);
-              });
-            });
-          }));
-        }
-
-        Promise.all(promises).then((asset) => {
-          /*
-          {
-            roi: contractVariables._grossReturn,
-            paybackMonths: contractVariables._paybackMonths,
-            walletAddress: contractVariables._from,
-            contractAddress: contractAddress._offerAddress,
-            --
-            assets: o.assets,
-            raisingAmount: o.offerRaisingAmount,
-            uuid: o.offerUuid,
-            companyName: o.firstName + ' ' + o.lastName,
-            companyLogo: o.picture,
-            companyUuid: o.uuid,
-            createdOn: o.createdOn,
-          }
-          */
-          const offer = {
-            roi: contractVariables._grossReturn / 10000,
-            paybackMonths: contractVariables._paybackMonths,
-            walletAddress: contractVariables._from,
-            contractAddress: contractVariables._offerAddress,
-            assets: asset
-          } as any;
-          this.offers.push(offer);
-          this.offerService.cacheOffers(this.offers);
-
-        });
-      }
-    });
+    const abi = (<any>SwapyExchange).abi;
+    const contract = new this.web3.eth.Contract((<any>SwapyExchange).abi, );
+  //
+  //   contract.getPastEvents('Offers', {
+  //     fromBlock: 0,
+  //     toBlock: 'latest'
+  //   }, (error, offersEvents) => {
+  //     for (const offersEvent of offersEvents) {
+  //       const contractVariables = offersEvent.returnValues;
+  //       // console.log(offersEvents);
+  //       const promises = [];
+  //       for (const assetAddress of contractVariables._assets) {
+  //         const contractAsset = new this.web3.eth.Contract((InvestmentAsset as any).abi, assetAddress);
+  //         promises.push(new Promise((resolve) => {
+  //           return contractAsset.methods.value().call().then(value => {
+  //             return contractAsset.methods.status().call().then(status => {
+  //               value = this.web3.utils.fromWei(value) * 340; // Temporary fixed value in Alpha
+  //               const offerAssets = {
+  //                   value,
+  //                   status
+  //               };
+  //               resolve(offerAssets);
+  //             });
+  //           });
+  //         }));
+  //       }
+  //
+  //       Promise.all(promises).then((asset) => {
+  //         /*
+  //         {
+  //           roi: contractVariables._grossReturn,
+  //           paybackMonths: contractVariables._paybackMonths,
+  //           walletAddress: contractVariables._from,
+  //           contractAddress: contractAddress._offerAddress,
+  //           --
+  //           assets: o.assets,
+  //           raisingAmount: o.offerRaisingAmount,
+  //           uuid: o.offerUuid,
+  //           companyName: o.firstName + ' ' + o.lastName,
+  //           companyLogo: o.picture,
+  //           companyUuid: o.uuid,
+  //           createdOn: o.createdOn,
+  //         }
+  //         */
+  //         const offer = {
+  //           roi: contractVariables._grossReturn / 10000,
+  //           paybackMonths: contractVariables._paybackMonths,
+  //           walletAddress: contractVariables._from,
+  //           contractAddress: contractVariables._offerAddress,
+  //           assets: asset
+  //         } as any;
+  //         this.offers.push(offer);
+  //         this.offerService.cacheOffers(this.offers);
+  //
+  //       });
+  //     }
+  //   });
   }
 
 
