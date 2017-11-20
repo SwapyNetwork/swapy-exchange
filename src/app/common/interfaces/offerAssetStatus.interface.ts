c600aba9996821be7399a1bdd853be90f8dd787f
Ícaro Harry <me@icaro.io>
2017-11-20 15:16:26 -0200

    Get company offers directly from blockchain

--- src/app/common/interfaces/offerAssetStatus.interface.ts
+++ src/app/common/interfaces/offerAssetStatus.interface.ts
@@ -1,11 +1,7 @@
-export const OPEN = 1;
-export const SOLD = 2;
-export const PENDING = 3;
+export const AVAILABLE = 0;
+export const PENDING_OWNER_AGREEMENT = 1;
+export const INVESTED = 2;
+export const RETURNED = 3;
+export const DELAYED_RETURN = 4;
 //
-export const TX_CREATION_PENDING = 0;
-export const TX_CREATED = 1;
-export const LOCKED = 2 ;
-export const TX_AGREEMENT_PENDING = 3;
-export const TX_AGREED = 4;
-export const TX_INVEST_PENDING = 5;
-export const TX_INVESTED = 6;
+export const PENDING_ETHEREUM_CONFIRMATION = -1;
--- src/app/credit-company/dashboard/dashboard.component.ts
+++ src/app/credit-company/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { OfferService } from './offer/offer.service';
 import { ExchangeProtocolService as ExchangeService } from '../../common/services/protocol/exchange.service';
 import { InvestmentAssetProtocolService as InvestmentAssetService } from '../../common/services/protocol/investment-asset.service';
 import { WalletService } from '../../common/services/wallet.service';
+import { Web3Service } from '../../common/services/web3.service';
 import { ErrorLogService } from '../../common/services/error-log.service';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { AGREE_INVESTMENT, TRANSFER_FUNDS, CREATE_OFFER } from '../../common/interfaces/events.interface';
@@ -20,6 +21,7 @@ export class DashboardComponent implements OnInit {
     private offerService: OfferService,
     private exchangeService: ExchangeService,
     private walletService: WalletService,
+    private web3Service: Web3Service,
     private toastr: ToastsManager, vcr: ViewContainerRef,
     private investmentAssetService: InvestmentAssetService,
     private errorLogService: ErrorLogService) {
@@ -30,25 +32,53 @@ export class DashboardComponent implements OnInit {
     this.updateOffers();
   }
 
-  updateOffers() {
-    this.offerService.getMyOffers().then(
-      (data: any) => {
-        this.offers = data.offers;
-      },
-      (error: any) => {
-        console.log(error)
-      }
-    );
-    let contract;
-    this.exchangeService.getMyOffers(this.walletService.getWallet().address, (err, offers) => {
-      offers.forEach(offer => {
+  private factoryOffers(offer) {
+    return new Promise(resolve => {
+      this.web3Service.getInstance().eth.getBlock(offer.blockHash).then(block => {
+        const newOffer = {
+          active: true,
+          assets: [],
+          companyName: offer.returnValues._from,
+          createdOn: (new Date(block.timestamp * 1000)).toISOString(),
+          paybackMonths: 0,
+          raisingAmount: 0,
+          roi: 0,
+          uuid: offer.returnValues._id,
+          walletAddress: offer.returnValues._from
+        };
         offer.returnValues._assets.forEach(asset => {
-          this.investmentAssetService.getConstants(asset, ['fixedValue', 'paybackDays']).then(obj => console.log(obj));
+          this.investmentAssetService.getConstants(asset, ['fixedValue', 'paybackDays', 'grossReturn', 'investor', 'status'])
+          .then(assetObj => {
+            const newAsset = {
+              contractAddress: asset,
+              investorWallet: (assetObj as any).investor,
+              status: (assetObj as any).status,
+              value: (assetObj as any).fixedValue / 100
+            };
+            newOffer.assets.push(newAsset);
+            if (!newOffer.raisingAmount) {
+              newOffer.paybackMonths = (assetObj as any).paybackDays;
+              newOffer.raisingAmount = (assetObj as any).fixedValue * 5 / 100;
+              newOffer.roi = (assetObj as any).grossReturn / 10000;
+            } else if (newOffer.assets.length === 5) {
+              resolve(newOffer);
+            }
+          });
         })
       });
-      console.log(offers);
     });
   }
 
-  getUpdatesFromBlockchain() {}
+  updateOffers() {
+    this.exchangeService.getMyOffers(this.walletService.getWallet().address, (err, offers) => {
+      const promises = [];
+      offers.forEach(offer => {
+        promises.push(this.factoryOffers(offer));
+      });
+
+      Promise.all(promises).then(resolvedOffers => {
+        this.offers = resolvedOffers;
+      });
+    });
+  }
 }
