2bc8a35742d5825739c8042039d276982c8b63a3
Ícaro Harry <me@icaro.io>
2017-11-29 17:39:19 -0200

    Update package.json and fix TS errors on window object

--- src/app/common/services/error-log.service.ts
+++ src/app/common/services/error-log.service.ts
@@ -14,9 +14,9 @@ export class ErrorLogService {
   }
 
   sendError() {
-    console.error(this.errorObject);
-    if (window.isElectron) {
-      this.electronService.ipcRenderer.send('log-error', this.errorObject);
+    // @TODO Implement third party error log service
+    if ((window as any).isElectron) {
+      console.log('log-error', this.errorObject);
     }
     this.delete();
   }
--- src/app/common/services/link.service.ts
+++ src/app/common/services/link.service.ts
@@ -7,7 +7,7 @@ export class LinkService {
   constructor(private electronService: ElectronService) {}
 
   public openLink(link: string) {
-    if (window.isElectron) {
+    if ((window as any).isElectron) {
       this.electronService.shell.openExternal(link);
     } else {
       window.open(link, '_blank');
