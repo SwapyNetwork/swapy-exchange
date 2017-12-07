8f02be27d9d86a0b52a578b89d609f647acb154a
Adriel <adrielgusa@gmail.com>
2017-12-07 17:22:09 -0200

    Added loading screen status bar is being reloaded

--- src/app/credit-company/credit-company.component.ts
+++ src/app/credit-company/credit-company.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
 import { CreditCompanyService } from './credit-company.service';
 import { WalletService } from '../common/services/wallet.service';
+import { LoadingService } from '../common/services/loading.service';
 import { ExchangeProtocolService } from '../common/services/protocol/exchange.service';
 import { InvestmentAssetProtocolService as AssetService } from '../common/services/protocol/investment-asset.service';
 import { INVESTED, RETURNED } from '../common/interfaces/offerAssetStatus.interface';
@@ -24,16 +25,16 @@ export class CreditCompanyComponent implements OnInit {
   public balance;
 
   constructor(private creditCompanyService: CreditCompanyService, private walletService: WalletService,
-    private exchangeProtocolService: ExchangeProtocolService, private assetService: AssetService) {};
+    private exchangeProtocolService: ExchangeProtocolService, private assetService: AssetService,
+    private loadingService: LoadingService) {};
 
   ngOnInit() {
     this.refreshStatusBar();
   };
 
   refreshStatusBar() { /**@todo Refresh via websocket when a investment is done */
-
+    this.loadingService.show();
     this.exchangeProtocolService.getMyOffers(this.walletService.getWallet().address, (error, offers) => {
-      console.log(offers);
       const promises = [];
       offers.forEach(offer => {
         promises.push(this.getStatistics(offer));
@@ -55,6 +56,7 @@ export class CreditCompanyComponent implements OnInit {
           .map(values => Number(values.fixedValue) + Number(values.fixedValue) * Number(values.grossReturn / 10000))
           .reduce((total: number, current: number) => (total + current), 0)) / 100;
         this.offersLength = offers.length;
+        this.loadingService.hide();
       });
     });
   }
