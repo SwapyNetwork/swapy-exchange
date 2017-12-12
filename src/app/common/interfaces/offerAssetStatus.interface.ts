de2342f1b677117140e2b7eabd03da7fa9926613
Adriel <adrielgusa@gmail.com>
2017-12-12 11:58:28 -0200

    Deleting old status from offerAssetStatus interface

--- src/app/common/interfaces/offerAssetStatus.interface.ts
+++ src/app/common/interfaces/offerAssetStatus.interface.ts
@@ -1,19 +1,9 @@
-export const AVAILABLE = 0;
-export const PENDING_OWNER_AGREEMENT = 1;
-export const INVESTED = 2;
-export const RETURNED = 3;
-export const DELAYED_RETURN = 4;
+export const AVAILABLE = 0
+export const PENDING_OWNER_AGREEMENT = 1
+export const INVESTED = 2
+export const FOR_SALE = 3
+export const PENDING_INVESTOR_AGREEMENT = 4
+export const RETURNED = 5
+export const DELAYED_RETURN = 6
 //
 export const PENDING_ETHEREUM_CONFIRMATION = -1;
-//
-export const OPEN = 1;
-export const SOLD = 2;
-export const PENDING = 3;
-//
-export const TX_CREATION_PENDING = 0;
-export const TX_CREATED = 1;
-export const LOCKED = 2 ;
-export const TX_AGREEMENT_PENDING = 3;
-export const TX_AGREED = 4;
-export const TX_INVEST_PENDING = 5;
-export const TX_INVESTED = 6;
