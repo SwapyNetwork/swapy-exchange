21ddf01aaafe74fb767eb60ff98c2231d58ae5d4
Adriel <adrielgusa@gmail.com>
2017-12-18 16:40:40 -0200

    Adding supply token function in protocol service

--- src/app/common/services/swapy-protocol.service.ts
+++ src/app/common/services/swapy-protocol.service.ts
@@ -118,6 +118,17 @@ export class SwapyProtocolService {
       });
   }
 
+  public supplyToken(contractAddress: string, value: number) {
+    this.AssetLibraryContract.options.address = contractAddress;
+    return this.AssetLibraryContract.methods
+      .supplyToken(value)
+      .send({
+        from: this.walletService.getWallet().address,
+        gas: 150000,
+        gasPrice: this.web3.utils.toWei(this.gasPrice, 'gwei')
+      });
+  }
+
   public get(event: string) {
     return this.SwapyExchangeContract.getPastEvents(event, {
       fromBlock: 0,
