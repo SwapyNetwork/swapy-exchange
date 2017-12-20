3fc1076b7eca79b0b522a0c06aa65cfa933491d3
Adriel <adrielgusa@gmail.com>
2017-12-20 18:59:01 -0200

    Adding message while and after confirming asset purchase

--- src/app/investor/confirm-purchase/confirm-purchase.component.ts
+++ src/app/investor/confirm-purchase/confirm-purchase.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MarketplaceService } from '../marketplace/marketplace.service';
 import { SwapyProtocolService as SwapyProtocol } from '../../common/services/swapy-protocol.service';
 import { ToastrService } from '../../common/services/toastr.service';
-
+import { SuccessfulInvestmentService } from './../successful-investment/successful-investment.service';
 
 @Component({
   selector: 'app-confirm-purchase',
@@ -16,6 +17,8 @@ export class ConfirmPurchaseComponent implements OnInit {
     private marketplaceService: MarketplaceService,
     private swapyProtocol: SwapyProtocol,
     private toastrService: ToastrService,
+    private successfulInvestmentService: SuccessfulInvestmentService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -23,11 +26,14 @@ export class ConfirmPurchaseComponent implements OnInit {
   }
 
   public async confirmPurchase() {
+    this.router.navigate(['investor/invest/success']);
     try {
       await this.swapyProtocol.buyAsset(this.asset.address, this.asset.value);
       this.toastrService.getInstance().success('Purchase requested');
+      this.successfulInvestmentService.setMessage('Your investment was mined by the Ethereum blockchain.');
     } catch (error) {
       this.toastrService.getInstance().error(error.message);
+      this.successfulInvestmentService.setErrorMessage(error.message);
     }
 
   }
