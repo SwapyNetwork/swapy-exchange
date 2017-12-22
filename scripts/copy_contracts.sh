263ed5f2469e1fe5080b39fbf59a1de1828e0667
Ícaro Harry <me@icaro.io>
2017-12-22 15:24:42 -0200

    Include sample dockerfile + copy electron script

--- scripts/copy_contracts.sh
+++ scripts/copy_contracts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm src/contracts/ganache/*
-cp ../../swapy-exchange-protocol/build/contracts/SwapyExchange.json src/contracts/ganache
-cp ../../swapy-exchange-protocol/build/contracts/InvestmentAsset.json src/contracts/ganache
-cp ../../swapy-exchange-protocol/build/contracts/AssetLibrary.json src/contracts/ganache
+cp ../swapy-exchange-protocol/build/contracts/SwapyExchange.json src/contracts/ganache
+cp ../swapy-exchange-protocol/build/contracts/InvestmentAsset.json src/contracts/ganache
+cp ../swapy-exchange-protocol/build/contracts/AssetLibrary.json src/contracts/ganache
