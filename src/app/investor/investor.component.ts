b1e46c6502d5746b77bd122e6ad1810ad8aeb6a4
Adriel <adrielgusa@gmail.com>
2018-01-10 19:27:43 -0200

    Update on the calculation of investor status bar

--- src/app/investor/investor.component.ts
+++ src/app/investor/investor.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { WalletService } from '../common/services/wallet.service';
 import { Web3Service } from '../common/services/web3.service';
 import { LoadingService } from '../common/services/loading.service';
-import { INVESTED, RETURNED, DELAYED_RETURN } from '../common/interfaces/offer-asset-status.interface';
+import { PENDING_OWNER_AGREEMENT, INVESTED, FOR_SALE, PENDING_INVESTOR_AGREEMENT, RETURNED,
+  DELAYED_RETURN } from '../common/interfaces/offer-asset-status.interface';
 import { DashboardService } from './dashboard/dashboard.service';
 
 
@@ -31,7 +32,7 @@ export class InvestorComponent implements OnInit {
     // this.refreshStatusBar();
   }
 
-  refreshStatusBar() {
+  public async refreshStatusBar() {
     this.loadingService.show();
 
     this.walletService.getEthBalance().then((balance) => {
@@ -53,38 +54,41 @@ export class InvestorComponent implements OnInit {
     this.returnedValue = 0;
     this.returnValue = 0;
 
-    this.investedValue = (assets.filter(asset => (asset.status === INVESTED ||
-      asset.status === RETURNED || asset.status === DELAYED_RETURN))
+    this.investedValue = (assets.filter(asset => (
+      Number(asset.status) >= PENDING_OWNER_AGREEMENT && Number(asset.status) <= DELAYED_RETURN))
       .map(asset => asset.value)
       .reduce((total, current) => (total + current), 0));
 
-    this.returnValue = assets.filter(asset => asset.status === INVESTED)
+    this.returnValue = assets.filter(asset => (
+      Number(asset.status) >= PENDING_OWNER_AGREEMENT && Number(asset.status) <= PENDING_INVESTOR_AGREEMENT))
       .map(asset => asset.value + asset.value * asset.grossReturn)
       .reduce((total, current) => (total + current), 0);
 
-    this.returnedValue = assets.filter(asset => (asset.status === RETURNED ||
-      asset.status === DELAYED_RETURN))
+    this.returnedValue = assets.filter(asset => (Number(asset.status) === RETURNED ||
+      Number(asset.status) === DELAYED_RETURN))
       .map(asset => asset.value + asset.value * asset.grossReturn)
       .reduce((total, current) => (total + current), 0);
 
-    const assetsLength = assets.filter(asset => Number(asset.status) === INVESTED).length;
-    this.averageReturn = assets.filter(asset => Number(asset.status) === INVESTED)
+    const assetsLength = assets.filter(asset => Number(asset.status) >= PENDING_OWNER_AGREEMENT &&
+      Number(asset.status) <= PENDING_INVESTOR_AGREEMENT).length;
+    this.averageReturn = assets.filter(asset => Number(asset.status) >= PENDING_OWNER_AGREEMENT &&
+      Number(asset.status) <= PENDING_INVESTOR_AGREEMENT)
       .map(asset => Number(asset.grossReturn))
       .reduce((total, current) => (total + current), 0);
 
     this.averageReturn = this.averageReturn === 0 ? 0 : (this.averageReturn / assetsLength).toFixed(4);
 
-    this.web3Service.getInstance().eth.getBlock('latest').then(block => {
-      const now = block.timestamp * 1000;
-      this.averagePaybackPeriod = assets.filter(asset => Number(asset.status) === INVESTED)
-        .map(asset => {
-          const returnDate = asset.investedAt.setMonth(asset.investedAt.getMonth() + asset.paybackDays / 30)
-          return Math.floor((returnDate - now) / (24 * 3600 * 1000));
-        })
-        .reduce((total, current) => (total + current), 0);
-        this.averagePaybackPeriod = this.averagePaybackPeriod === 0 ? 0 : this.averagePaybackPeriod  / assetsLength;
-        this.loadingService.hide();
-    });
+    const block = (await this.web3Service.getInstance().eth.getBlock('latest'));
+    const now = block.timestamp * 1000;
+    this.averagePaybackPeriod = assets.filter(asset =>
+      Number(asset.status) >= PENDING_OWNER_AGREEMENT && Number(asset.status) <= PENDING_INVESTOR_AGREEMENT)
+      .map(asset => {
+        const returnDate = asset.investedAt.setMonth(asset.investedAt.getMonth() + asset.paybackDays / 30)
+        return Math.floor((returnDate - now) / (24 * 3600 * 1000));
+      })
+      .reduce((total, current) => (total + current), 0);
+    this.averagePaybackPeriod = this.averagePaybackPeriod === 0 ? 0 : Math.round(this.averagePaybackPeriod  / assetsLength);
+    this.loadingService.hide();
     this.assetsLength = assets.length;
   };
 }
