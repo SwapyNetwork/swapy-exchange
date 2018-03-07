f07acc915b19bcaa300ebec5bf31a46abaafce69
Adriel <adrielgusa@gmail.com>
2018-03-07 18:25:36 -0300

    Adding script to copy files from Docker

---
+++ scripts/copy_contracts_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf src/contracts/ganache
+mkdir src/contracts/ganache 
+#$1 = Docker container name , i.e, stranger_name
+sudo docker cp $1:home/adriel/www/swapy-exchange-protocol/build/contracts/SwapyExchange.json  src/contracts/ganache/
+sudo docker cp $1:home/adriel/www/swapy-exchange-protocol/build/contracts/InvestmentAsset.json src/contracts/ganache/
+sudo docker cp $1:home/adriel/www/swapy-exchange-protocol/build/contracts/AssetLibrary.json src/contracts/ganache/
+sudo docker cp $1:home/adriel/www/swapy-test-faucet/build/contracts/Token.json src/contracts/ganache/
