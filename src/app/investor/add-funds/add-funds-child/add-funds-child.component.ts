ab49968c363b693b726074315c286a5d95e1f451
Adriel <adrielgusa@gmail.com>
2018-03-27 20:42:24 -0300

    Adding QRCode in Add funds page

--- src/app/investor/add-funds/add-funds-child/add-funds-child.component.ts
+++ src/app/investor/add-funds/add-funds-child/add-funds-child.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletService } from '../../../common/services/wallet.service';
-
+const qrcodeGenerator = require("qrcode-generator");
 @Component({
   selector: 'app-add-funds-child',
   templateUrl: './add-funds-child.component.html',
@@ -10,11 +10,19 @@ export class AddFundsChildComponent implements OnInit {
 
   public wallet;
   constructor(
-    private walletService: WalletService
+    private walletService: WalletService,
   ) { }
 
   async ngOnInit() {
     this.wallet = await this.walletService.getWallet();
+    this.getQRUri();
+  }
+
+  public getQRUri() {
+    const qr = qrcodeGenerator(0, 'H');
+    qr.addData(this.wallet.address);
+    qr.make();
+    document.getElementById('qrcode').innerHTML = qr.createImgTag(5); // NOT PROPER ANGULAR 2
   }
 
 }
